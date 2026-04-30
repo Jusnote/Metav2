43d0534324c21943a34319ec710f1a4a5820d129
Aldemir <aldemirlima.adv@hotmail.com>
2026-04-30 18:43:22 -0300

    feat(questoes): barrel export de filtros/shared

---
+++ src/components/questoes/filtros/shared/index.ts
@@ -0,0 +1,11 @@
+export { FilterCheckboxItem } from './FilterCheckboxItem';
+export type { FilterCheckboxItemProps } from './FilterCheckboxItem';
+
+export { FilterAlphabeticList } from './FilterAlphabeticList';
+export type {
+  FilterAlphabeticListProps,
+  AlphabeticItem,
+} from './FilterAlphabeticList';
+
+export { FilterRecentesBlock } from './FilterRecentesBlock';
+export type { FilterRecentesBlockProps } from './FilterRecentesBlock';
